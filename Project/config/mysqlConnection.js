0edf5e0172a7f3a6ea26d78d2610a2c17afe9653
dat <mynameisdat6@gmail.com>
2020-03-22 17:12:11 +0700

    change db name to etutor

--- Project/config/mysqlConnection.js
+++ Project/config/mysqlConnection.js
@@ -4,7 +4,7 @@ const connection = mysql.createConnection({
   host: process.env.DB_HOST || "localhost",
   user: process.env.DB_USER || "root",
   password: process.env.DB_PASSWORD || "",
-  database: process.env.DB_NAME || "Project"
+  database: process.env.DB_NAME || "etutor"
 })
 connection.connect(function(err){
   if(err) throw err;

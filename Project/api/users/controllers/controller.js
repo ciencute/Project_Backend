d528ea56f0a6ce71a52f2d893947bc8ad7f0f75c
dat <mynameisdat6@gmail.com>
2020-03-23 03:19:19 +0700

    loz

--- Project/api/users/controllers/controller.js
+++ Project/api/users/controllers/controller.js
@@ -1,8 +1,10 @@
 const Users = require ('../models/model');
 exports.List_all_user = (req, res) => {
-  Users.getAllUsers ((err, users) => {
-    if (err) res.send (err);
-    console.log('this is request:'+ req.params);
-    res.json (res.body);
-  });
+  // Users.getAllUsers ((err, users) => {
+  //   if (err) res.send (err);
+  //   console.log('this is request:'+ req.params);
+  //
+  // });
+  console.log('this is request:'+ req.body);
+  res.json (res.body);
 };

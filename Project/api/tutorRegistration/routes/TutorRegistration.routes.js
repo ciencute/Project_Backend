b11f42bdc9d3ad00cb246de8a1668c60fdf2fe36
dat <mynameisdat6@gmail.com>
2020-03-25 19:37:42 +0700

    add TutorRegistration.controller.js

--- Project/api/tutorRegistration/routes/TutorRegistration.routes.js
+++ Project/api/tutorRegistration/routes/TutorRegistration.routes.js
@@ -1,4 +1,5 @@
 module.exports = app => {
 	const tutorRegistrationController = require("../controller/TutorRegistration.controller");
+	const jwt = require('../../../MiddleWares/jwtMiddleware');
 	app.get('/tutorregistration', tutorRegistrationController.ListAllRegistraion )
 };
--- Project/server.js
+++ Project/server.js
@@ -3,7 +3,7 @@ let bodyparser = require ('body-parser');
 let app = express ();
 app.use (bodyparser.urlencoded ({extended: true}));
 app.use (bodyparser.json ());
-app.use
+
 let port = process.env.PORT || 3000;
 console.log('RESTful API server started on: ' + port);
 
